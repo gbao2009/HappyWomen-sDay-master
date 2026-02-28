f9237ea2283b99ad23e6e632f17a9717ac989aca
gbao2009 <162603754+gbao2009@users.noreply.github.com>
2026-02-28 15:17:41 +0000

    uu

--- app.js
+++ app.js
@@ -15,71 +15,72 @@ var progressLoad = setInterval(progressInterva,100)
 
 button.addEventListener("click", function(){
 
-    // phát nhạc
-    let music = document.getElementById("bgm");
-    music.play();
+// phát nhạc
+let music = document.getElementById("bgm");
+music.play();
 
-    // lưu trạng thái đang phát
-    localStorage.setItem("musicPlaying", "true");
+// lưu trạng thái đang phát
+localStorage.setItem("musicPlaying", "true");
 
-    button.style.transform = "scale(0.8)";
+button.style.transform = "scale(0.8)";
 
-    setTimeout(()=>{
-        window.location.href = "./love/love.html";
-    },200);
+setTimeout(()=>{
+window.location.href = "./love/love.html";
+},200);
 
 });
 
 function progressInterva(){
-    if(count == 100 && percentWidth == 100){
-        clearInterval(progressLoad)
-        percent.textContent = "Ok rùi đó:)";
-        percent.style.letterSpacing = "1px";
-        textBox.style.transform = "scale(1.3)"
-        heartItem3.style.animation = "1s heartScale forwards"
-        setTimeout(()=>{
-            textBox.style.transform = "scale(0)"
-        },400)
-        setTimeout(()=>{
-            textBox.style.opacity = "0"
-        },600)
-        setTimeout(()=>{
-            button.style.transform= "scale(1)";
-        },800);
-        setTimeout(()=>{
-            button.style.background = "rgb(244,118,121)"
-            button.style.width = "130px";
-            button.style.borderRadius = "20px"
-        },1500)
-        setTimeout(()=>{
-            button.style.height = "40px";
-        },2000)
-        setTimeout(()=>{
-            textBtn.textContent = "Click me!"
-            textBtn.style.color = "#fff"
-        },2500)
-        setTimeout(()=>{
-            cursor.style.opacity = "1";
-        },3000)
-    }
-    else{
-        if(count == 10){
-            heartItem1.style.animation = "1s heartScale forwards"
-        }
-        if(count ==46){
-            percent.style.color= "#fff"
-        }
-        if(count == 60){
-            heartItem2.style.animation = "1s heartScale forwards"
-        }
-        count += 1;
-        percentWidth += 1;
-        heartLeft += 0.968;
-        progress.style.width = percentWidth + '%'
-        percent.innerText = count + '%'
-        heartBox.style.left = heartLeft + '%'
-    }
+if(count == 100 && percentWidth == 100){
+clearInterval(progressLoad)
+percent.textContent = "Ok rùi đó:)";
+percent.style.letterSpacing = "1px";
+textBox.style.transform = "scale(1.3)"
+heartItem3.style.animation = "1s heartScale forwards"
+setTimeout(()=>{
+textBox.style.transform = "scale(0)"
+},400)
+setTimeout(()=>{
+textBox.style.opacity = "0"
+},600)
+setTimeout(()=>{
+button.style.transform= "scale(1)";
+},800);
+setTimeout(()=>{
+button.style.background = "rgb(244,118,121)"
+button.style.width = "130px";
+button.style.borderRadius = "20px"
+},1500)
+setTimeout(()=>{
+button.style.height = "40px";
+},2000)
+setTimeout(()=>{
+textBtn.textContent = "Click me!"
+textBtn.style.color = "#fff"
+},2500)
+setTimeout(()=>{
+cursor.style.opacity = "1";
+},3000)
 }
+else{
+if(count == 10){
+heartItem1.style.animation = "1s heartScale forwards"
+}
+if(count ==46){
+percent.style.color= "#fff"
+}
+if(count == 60){
+heartItem2.style.animation = "1s heartScale forwards"
+}
+count += 1;
+percentWidth += 1;
+heartLeft += 0.968;
+progress.style.width = percentWidth + '%'
+percent.innerText = count + '%'
+heartBox.style.left = heartLeft + '%'
+}
+}
+
 setInterval(()=>{
     let img = document.createElement("div");
     img.className = "imgRain";
