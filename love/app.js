d4a7c0b76758c80f469e9c7701fe45eda9d56f53
gbao2009 <162603754+gbao2009@users.noreply.github.com>
2026-02-28 06:15:29 +0000

    qqqqqqqqqq

--- love/app.js
+++ love/app.js
@@ -80,3 +80,22 @@ function progressInterva(){
         heartBox.style.left = heartLeft + '%'
     }
 }
+setInterval(()=>{
+    let img = document.createElement("div");
+    img.className = "imgRain";
+
+    img.innerHTML = `<img src="image/love3.jpg" width="80">`;
+
+    document.body.appendChild(img);
+
+    img.style.left = Math.random()*100 + "%";
+
+    setTimeout(()=>{
+        img.style.top = "100%";
+    },50);
+
+    setTimeout(()=>{
+        img.remove();
+    },5000);
+
+},1000);
